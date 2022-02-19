c39110d3d372624fd34200a87261c69494f1d94d
kee-reel <c4rb0n_unit@protonmail.com>
2022-02-19 23:32:10 +0300

    Remove leftovers

--- main.asm
+++ main.asm
@@ -1,8 +1,6 @@
 extern printf
 section .data
 	msg_fmt db "Message: %d", 10, 0
-	msg db "Hello world!", 0
-	msg_len db $-msg-1
     value dd 1337
 section .bss
 section .text
@@ -18,15 +16,5 @@ main:
 
     mov rsp, rbp
     pop rbp
-
-	;mov rax, 1	; write
-	;mov rdi, 1	; to stdout
-	;mov rsi, msg	; set msg
-	;mov rdx, msg_len	; msg len
-	;syscall
-
-	;mov rax, 60	; exit
-	;mov rdi, 1	; exit code
-	;syscall
 
     ret
